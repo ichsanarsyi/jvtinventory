1b2182bd942272dfedb31e500891da4d90d1ef5a
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2021-03-31 00:27:24 +0700

    Info Home

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -1,28 +1,71 @@
 @extends('layout.v_template')
 @section('headertitle')
 @section('title','Home')
-<h3 style="margin:-0px;">
+<h3 style="margin:0px; margin-left:0px;">
   @yield('title')
 </h3>
 @endsection
 @section('content')
 <div class="row">
-    <div class="col-md-8 pull-left">
-        @if (session('pesan'))
+  <div class="col-sm-12">
+    @if (session('pesan'))
+    <div class="alert alert-success alert-dismissible">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+      <h4><i class="icon fa fa-check"></i> Sukses!</h4>
+      {{ session('pesan') }}
+    </div>
+    @endif
+        @if (session('message'))
         <div class="alert alert-success alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h4><i class="icon fa fa-check"></i> Sukses!</h4>
-            {{ session('pesan') }}
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <h4><i class="icon fa fa-check"></i> Login Berhasil</h4>
+          {{ session('message') }}
         </div>
         @endif
-        @if (session('message'))
-            <div class="alert alert-success alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <h4><i class="icon fa fa-check"></i> Login Berhasil</h4>
-            {{ session('message') }}
-            </div>
-        @endif
-    </div>
+  </div>
+  
+
+  <div class="col-md-8 pull-left">
+        <div class="box box-info">
+          <div class="box-header with-border">
+            <i class="fa fa-bar-chart"></i>
+                <h3 class="box-title">Info</h3>
+          </div>
+          <div class="box-body">
+            <div class="col-sm-4">
+                <div class="info-box bg-purple">
+                    <span class="info-box-icon bg-purple-active"><i class="fa fa-briefcase"></i></span>
+                    <div class="info-box-content">
+                        <span style="margin-top: 7px;" class="info-box-text">Total<br>Hardware</span>
+                        <span class="info-box-number"><?php $totalHW = DB::table('tbl_hardware')->count(); echo $totalHW;?></span>
+                    </div><!-- /.info-box-content -->
+                </div><!-- /.info-box -->
+            </div><!-- /.col -->
+            <div class="col-sm-4">
+                <div class="info-box bg-blue">
+                    <span class="info-box-icon bg-blue-active"><i class="fa fa-cloud"></i></span>
+                    <div class="info-box-content">
+                        <span style="margin-top: 7px;" class="info-box-text">Total<br>Software</span>
+                        <span class="info-box-number"><?php $totalHW = DB::table('tbl_software')->count(); echo $totalHW;?></span>
+                    </div><!-- /.info-box-content -->
+                </div><!-- /.info-box -->
+            </div><!-- /.col -->
+            <div class="col-sm-4">
+                <div class="info-box bg-navy">
+                    <span class="info-box-icon bg-navy-active"><i class="glyphicon glyphicon-user"></i></span>
+                    <div class="info-box-content">
+                        <span style="margin-top: 7px;" class="info-box-text">Total<br>User</span>
+                        <span class="info-box-number"><?php $totalHW = DB::table('users')->count(); echo $totalHW;?></span>
+                    </div><!-- /.info-box-content -->
+                </div><!-- /.info-box -->
+            </div><!-- /.col -->
+          </div>
+          {{-- box body --}}
+          </div>
+        </div>
+
+
+
     <div class="col-md-4 pull-right">
         <div class="box box-info">
             <div class="box-header with-border">
@@ -35,7 +78,7 @@
             <!-- /.box-header -->
             <div class="box-body">
                     @foreach ($catatan as $data)
-                    <div class="callout callout-info">
+                    <div class="callout callout-default bg-aqua-active">
                     @if (auth()->user()->level == 'Admin') 
                     <i style="cursor: pointer;" data-toggle="modal" data-target="#modal-delete{{$data->id_catatan}}" class="fa fa-trash pull-right"> </i>
                     <i style="cursor: pointer; padding-right:10px;" data-toggle="modal" data-target="#modal-edit{{$data->id_catatan}}" class="fa fa-pencil pull-right"> </i>
